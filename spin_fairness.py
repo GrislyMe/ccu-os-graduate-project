5b6c2dec6fcbec9426bfc56b8f705b3c8f45f99f
GrislyMe <grislyme@csie.io>
2021-10-05 23:15:36 +0800

    lock code clean up and plot generate

--- spin_fairness.py
+++ spin_fairness.py
@@ -6,10 +6,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 import webbrowser
 
-def getCost():
+def getCost(file_name: str):
     ret = []
-    merge = 2
-    with open("cost", "r") as inputFile:
+    merge = 4
+    with open(file_name, "r") as inputFile:
         for line in inputFile.readlines():
             ret += [list(map(int, line.split()[1:]))]
     return np.array([np.sum(np.array(ret)[i - merge: i], axis=0) for i in range(merge, 100, merge)])
@@ -21,16 +21,32 @@ def main():
         print("failed to get vcore_number")
         return
 
-    cost = getCost();
-    #plt.figure(figsize=(24,4))
 
-    plt.imshow(cost)
 
-    plt.tick_params(labelleft=False, labelright=False,
+    fig, ax = plt.subplots(1, 3)
+
+    ax[0].imshow(getCost("plock_cost"), label = "plock")
+    ax[0].tick_params(labelleft=False, labelright=False,
                    labeltop=False, labelbottom=False,
                    left=False, right=False,
                    top=False, bottom=False)
+    ax[0].set_title("Plock")
 
+    ax[1].imshow(getCost("ticket_cost"))
+    ax[1].tick_params(labelleft=False, labelright=False,
+                   labeltop=False, labelbottom=False,
+                   left=False, right=False,
+                   top=False, bottom=False)
+    ax[1].set_title("Ticket Lock")
+
+    ax[2].imshow(getCost("ticket_cost"))
+    ax[2].tick_params(labelleft=False, labelright=False,
+                   labeltop=False, labelbottom=False,
+                   left=False, right=False,
+                   top=False, bottom=False)
+    ax[2].set_title("MCS Lock")
+
+    fig.suptitle('Fairness Test',fontsize=16) 
     plt.show()
 
     if upload:
--- ticket_lock.c
+++ ticket_lock.c
@@ -1,63 +1,58 @@
 #define _GNU_SOURCE
-#include <assert.h>
-#include <math.h>
 #include <pthread.h>
-#include <signal.h>
 #include <stdatomic.h>
 #include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <strings.h>
-#include <sys/syscall.h>
-#include <sys/sysinfo.h>
 #include <threads.h>
 #include <unistd.h>
 
-#define num_of_vcore 6
+#define num_of_vcore 16
 
-int num_of_thread;
 atomic_ulong gtk = 0;
-atomic_ulong srv = 0;
+unsigned long srv = 0;
 int counter[100][num_of_vcore] = {0};
-struct timespec thread_time[64];
+int globalData[300];
 
-void ticket_lock(){
-    atomic_ulong ltk = gtk++;
-    while(atomic_load(&ltk) != srv)
-        asm("pause");
+void ticket_lock() {
+	unsigned long ltk = atomic_fetch_add(&gtk, 1);
+	while (ltk != srv)
+		asm("pause");
 }
 
-void ticket_unlock(){
-    srv++;
+void ticket_unlock() {
+	srv++;
 }
 
 void thread() {
-    int cpu;
-    struct timespec start, current;
-    clock_gettime(CLOCK_REALTIME, &start);
-    clock_gettime(CLOCK_REALTIME, &current);
-    int diff = current.tv_sec - start.tv_sec;
+	int cpu;
+	struct timespec start, current;
+	clock_gettime(CLOCK_REALTIME, &start);
+	clock_gettime(CLOCK_REALTIME, &current);
+	int diff = current.tv_sec - start.tv_sec;
 
-    while(diff < 100){
-    	ticket_lock(); // lock
-        
-        // CS
-        cpu = sched_getcpu();
-        counter[diff][cpu]++;
-        // CS
+	while (diff < 100) {
+		ticket_lock();  // lock
 
-        ticket_unlock(); // unlock
-        
-        clock_gettime(CLOCK_REALTIME, &current);
-        diff = current.tv_sec - start.tv_sec;
-    }
+		// CS
+		cpu = sched_getcpu();
+		counter[diff][cpu]++;
+		for (int i = 0; i < 300; i++) {
+			globalData[i] += i;
+		}
+		// CS
+
+		ticket_unlock();  // unlock
+
+		clock_gettime(CLOCK_REALTIME, &current);
+		diff = current.tv_sec - start.tv_sec;
+	}
 }
 
 int main() {
 	// init
-	num_of_thread = num_of_vcore * num_of_vcore;	
+	int num_of_thread = num_of_vcore;  // * num_of_vcore;
 
 	// create and join thread
 	pthread_t* tid = (pthread_t*)malloc(sizeof(pthread_t) * num_of_thread);
@@ -74,17 +69,17 @@ int main() {
 	// pthread_spin_destroy(&lock);
 
 	FILE* out = fopen("ticket_cost", "w");
-    if(!out){
-        fprintf(stderr, "fail to open file\n");
-        exit(0);
-    }
-    for(int i = 0; i < 100; i++){
-        fprintf(out, "#%d ", i);
-        for(int k = 0; k < num_of_vcore; k++){
-            fprintf(out, "%d ", counter[i][k]);
-        }
-        fprintf(out, "\n");
-    }
-    fclose(out);
+	if (!out) {
+		fprintf(stderr, "fail to open file\n");
+		exit(0);
+	}
+	for (int i = 0; i < 100; i++) {
+		fprintf(out, "#%d ", i);
+		for (int k = 0; k < num_of_vcore; k++) {
+			fprintf(out, "%d ", counter[i][k]);
+		}
+		fprintf(out, "\n");
+	}
+	fclose(out);
 	return 0;
 }
