a16fc860548acbb0ff3660d6600061c8fd2eb71d
playone <kpu107u@csie.io>
2021-10-16 02:42:11 +0800

    SoA remain section test

--- misc/routing_spinlock.c
+++ misc/routing_spinlock.c
@@ -15,77 +15,126 @@
 #include <threads.h>
 #include <unistd.h>
 
-// 0, 3, 5, 6, 2, 4, 7, 1, 9, 14, 10, 13, 15, 8, 11, 12, 0
-int idCov[16] = {3, 9, 4, 5, 7, 6, 2, 1, 11, 14, 13, 12, 0, 15, 10, 8};  // this array should be changed on different CPU
+#define RS_size 100
+#define num_of_vcore 6
+
+// 0 -> 4 -> 5 -> 3 -> 2 -> 1 -> 0
+int idCov[6] = {4, 0, 1, 2, 5, 3};  // this array should be changed on different CPU
+int rs_set[] = {160000, 120000, 80000, 40000, 20000, 10000, 5000, 1000, 500, 100};
 thread_local int routingID;
-volatile atomic_int waitArray[16];
-volatile atomic_int GlobalLock = 0;
-int zero = 0;
-int num_of_vcore;
-int num_of_thread;
+atomic_int GlobalLock = 0;
+static int zero = 0;
+long long int counter;
+int globalData[300];
+
+atomic_int waitArray[6];
+
+typedef struct info{
+    int rs_label;
+    int cid;
+}info;
 
 void spin_init() {
-	routingID = idCov[sched_getcpu()];
-	for (int i = 0; i < num_of_vcore; i++) {
-		atomic_init(waitArray + i, 0);
-	}
+    for(int i = 0; i < num_of_vcore; i++){
+        waitArray[i] = 0;    
+    }
 }
 
-void spin_lock() {
-	waitArray[routingID] = 1;
-	while (atomic_load(waitArray + routingID) == 1) {
-		zero = 0;
-		if (atomic_compare_exchange_strong(&GlobalLock, &zero, 1)) {
-			atomic_store(waitArray + routingID, 0);
-			return;
-		}
-	}
-	atomic_store(waitArray + routingID, 0);
-	return;
+void spin_lock(int routingID) {
+    waitArray[routingID] = 1;
+    while(1){
+        zero = 0; // let the variable "zero" always contain the value '0'
+        if(waitArray[routingID] == 0)
+            return;
+        if(atomic_compare_exchange_strong(&GlobalLock, &zero, 1)){
+            waitArray[routingID] = 0;
+            return;
+        }
+    }
 }
 
-void spin_unlock() {
-	// the digit 6 is my core number
-	// it should be changed on different CPU
-	for (int i = 1; i < num_of_vcore; i++) {
-		if (waitArray[(i + routingID) % 16] == 1) {
-			atomic_store(waitArray + (i + routingID) % 16, 0);
-			return;
-		}
-	}
-	GlobalLock = 0;
+void spin_unlock(int routingID) {	
+	for(int i = 0; i < num_of_vcore - 1; i++){
+        if(waitArray[(i + routingID) % num_of_vcore] == 1){
+            waitArray[(i + routingID) % num_of_vcore] = 0;
+            return;
+        }
+    }
+    GlobalLock = 0;
 }
 
-void thread() {
-	routingID = idCov[sched_getcpu()];
+void thread(void* args) {
+    int cid = ((info*)args)->cid;
+    int rsID = ((info*)args)->rs_label;
+    routingID = idCov[cid];
+    cpu_set_t cpuset;
+    CPU_ZERO(&cpuset);
+    CPU_SET(cid, &cpuset);
+    sched_setaffinity(0, sizeof(cpuset), &cpuset);
 
-	spin_lock();
-	printf("routingID : %d has CS\n", routingID);
-	sleep(1);
-	spin_unlock();
-	printf("routingID : %d unlock\n", routingID);
+    struct timespec start;
+    struct timespec current;
+    struct timespec rs_start;
+    struct timespec rs_end;
+    int diff;
+    int rs = rs_set[rsID];
+
+    printf("rs: %d\n", rs);
+    printf("%d\n", cid);
+    clock_gettime(CLOCK_REALTIME, &start);
+    clock_gettime(CLOCK_REALTIME, &current);
+    diff = current.tv_sec - start.tv_sec;
+    while(diff < 10){
+        spin_lock(routingID); // lock
+        //CS
+        for(int i = 0; i < 300; i++)
+            globalData[i] += i;
+        counter++;
+        //CS
+        spin_unlock(routingID); // unlock
+        clock_gettime(CLOCK_REALTIME, &rs_start);
+        while(clock_gettime(CLOCK_REALTIME, &rs_end) && (rs_end.tv_nsec - rs_start.tv_nsec) < rs)
+            ;
+        clock_gettime(CLOCK_REALTIME, &current);
+        diff = current.tv_sec - start.tv_sec;
+    }
 }
 
 int main() {
 	// init
-	num_of_vcore = get_nprocs();
-	num_of_thread = num_of_vcore;
+	int num_of_thread = num_of_vcore;
+    info args[6];
+    for(int i = 0; i < 6; i++){
+        args[i].cid = i;
+    }
+
 	spin_init();
 
-	// create and join thread
-	pthread_t* tid = (pthread_t*)malloc(sizeof(pthread_t) * num_of_thread);
+    for(int j = 0; j < 10; j++){
+    	// create and join thread
+        counter = 0;
+        printf("%d\n", rs_set[j]);
+	    pthread_t* tid = (pthread_t*)malloc(sizeof(pthread_t) * num_of_thread);
 
-	// pthread_spin_lock(&lock);
-	for (int i = 0; i < num_of_thread; i++) {
-		pthread_create(&tid[i], NULL, (void*)thread, NULL);
-	}
-	// pthread_spin_unlock(&lock);
+	    // pthread_spin_lock(&lock);
+    	for (int i = 0; i < num_of_thread; i++) {
+            args[i].rs_label = j;
+    		pthread_create(&tid[i], NULL, (void*)thread, &args[i]);
+	    }
+    	// pthread_spin_unlock(&lock);
+	    for (int i = 0; i < num_of_thread; i++)
+    		pthread_join(tid[i], NULL);
 
-	for (int i = 0; i < num_of_thread; i++)
-		pthread_join(tid[i], NULL);
-
+        FILE* out = fopen("SoA_lps", "a");
+        if(!out){
+            fprintf(stderr, "fail to open file\n");
+            return 0;
+        }
+        fprintf(out, "%d %lld\n", rs_set[j], counter);
+        fclose(out);
+    }
+    
 	// pthread_spin_destroy(&lock);
 
-	printf("global lock : %d\n", GlobalLock);
 	return 0;
 }
