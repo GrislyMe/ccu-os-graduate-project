4aabc85d4b793c19546dac72ecd69c34019ffb96
mrp1944 <mrp1944@csie.io>
2021-10-18 20:23:12 +0800

    MCS_RS is done

--- mcs/MCS_lock_test_RS.c
+++ mcs/MCS_lock_test_RS.c
@@ -9,9 +9,11 @@
 int RS_size, nowRS;
 int RS_list[10] = {160000, 120000, 80000, 40000, 20000, 10000, 5000, 1000, 500, 100};
 int num_of_thread;
-long long int counter[10][num_of_vcore] = {0};
+long long int counter[10] = {0};
 int globalData[300];
 struct timespec thread_time[64];
+int test;
+int lt, ut;
 
 void thread() {
 	mcs_node* node = malloc(sizeof(mcs_node));
@@ -22,14 +24,17 @@ void thread() {
 	clock_gettime(CLOCK_REALTIME, &start);
 	clock_gettime(CLOCK_REALTIME, &current);
 	int diff = current.tv_sec - start.tv_sec;
-    printf("while begin\n");
 	while (diff < 10) {
-		spin_lock(node);  // lock
-		
-		// CS
+		spin_lock(node);  // lock	
 		cpu = sched_getcpu();
-		counter[nowRS][cpu]++;
+		// CS
+		counter[nowRS]++;
+		//printf("++ [%d]=%lld\n", nowRS, counter[nowRS]);
 		for (int i = 0; i < 300; i++) {
+			/*if(i > 100)
+				globalData[i] += i / 127 + i * 2 - 11;
+			if(i > 200)
+				globalData[i] += i*i / 1337;*/
 			globalData[i] += i;
 		}
 		// CS
@@ -37,15 +42,17 @@ void thread() {
 		clock_gettime(CLOCK_REALTIME, &t);
 		while(1){
 			clock_gettime(CLOCK_REALTIME, &current);
-			if(current.tv_sec - t.tv_sec > RS_size){
+			if(current.tv_nsec - t.tv_nsec > RS_size){
+				break;
+			}
+			if(current.tv_nsec - t.tv_nsec < 0){
+				////I don't know why but it really happend
 				break;
 			}
 		}
 		clock_gettime(CLOCK_REALTIME, &current);
 		diff = current.tv_sec - start.tv_sec;
-        printf("while end\n");
 	}
-    printf("thread end\n");
 	free(node);
 	return;
 }
@@ -53,9 +60,12 @@ void thread() {
 int main() {
 	// init
 	num_of_thread = num_of_vcore;
+	tail = malloc(sizeof(mcs_node));
+	lt=0, ut=0;
 	for(int T=0;T<10;T++){
 		nowRS = T;
 		RS_size = RS_list[nowRS];
+		atomic_init(&tail, mcs_null);
 		// create and join thread
 		pthread_t* tid = (pthread_t*)malloc(sizeof(pthread_t) * num_of_thread);
 		for (int i = 0; i < num_of_thread; i++) {
@@ -64,25 +74,17 @@ int main() {
 	
 		for (int i = 0; i < num_of_thread; i++)
 			pthread_join(tid[i], NULL);
-		free(tail);
-        printf("#%dDone\n", T);
 	}
+	free(tail);
 	// printf("lock is all done\n");
 	// lock program is done, print out result
-	long long total = 0;
 	FILE* out = fopen("MCS_RS_test", "w");
 	if (!out) {
 		printf("fail to open file\n");
 		return 0;
 	}
 	for(int T=0;T<10;T++){
-		fprintf(out, "%d ", RS_list[T]);
-		total = 0;
-		for (int k = 0; k < num_of_vcore; k++) {
-			total += counter[T][k];
-			//fprintf(out, "%d ", counter[k]);
-		}
-		fprintf(out, "%lld\n", total);
+		fprintf(out, "%d %lld\n", RS_list[T], counter[T]);
 	}
 	fclose(out);
 	return 0;
