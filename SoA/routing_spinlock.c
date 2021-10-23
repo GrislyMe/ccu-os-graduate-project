aff1edcdcfd61d745da4b3dfe6edfbcf9dcb4497
playone <kpu107u@csie.io>
2021-10-24 03:48:52 +0800

    corrects mistakes

--- SoA/routing_spinlock.c
+++ SoA/routing_spinlock.c
@@ -5,18 +5,19 @@
 #include <stdlib.h>
 #include <threads.h>
 
-#define num_of_vcore 16
+#define num_of_vcore 6
 
 // 2 -> 1 -> 0 -> 4 -> 5 -> 3
 // ---------->    <----------
 //  same ccx        same ccx
-// int idCov[num_of_vcore] = {3, 0, 4, 5, 7, 6, 2, 1, 15, 12, 14, 8, 11, 10, 9, 13};
-int idCov[num_of_vcore] = {3, 0, 4, 5, 7, 6, 2, 1, 11, 14, 13, 12, 9, 15, 10, 8};
+//int idCov[num_of_vcore] = {3, 0, 4, 5, 7, 6, 2, 1, 11, 14, 13, 12, 9, 15, 10, 8};
 // this array should be changed on different CPU
+int idCov[6] = {4, 0, 1, 2, 5, 3};
 int rs_set[] = {160000, 120000, 80000, 40000, 20000, 10000, 5000, 1000, 500, 100};
 atomic_llong counter = 0;
 // atomic_llong counter;
 int globalData[300] = {0};
+struct timespec start;
 
 void thread(info* args) {
 	int cid = args->cid;
@@ -26,29 +27,31 @@ void thread(info* args) {
 	CPU_SET(cid, &cpuset);
 	sched_setaffinity(0, sizeof(cpuset), &cpuset);
 
-	struct timespec start;
 	struct timespec current;
 	struct timespec rs_start;
-	int diff;
+    struct timespec rs_end;
 	int rs = rs_set[rsID];
 
-	clock_gettime(CLOCK_REALTIME, &start);
-	clock_gettime(CLOCK_REALTIME, &current);
-	diff = time_diff(start, current).tv_sec;
-	while (diff < 10) {
+	while (1) {
+        clock_gettime(CLOCK_REALTIME, &current);
+        if(time_diff(start, current).tv_sec > 10)
+            break;
+
 		spin_lock();  // lock
 		// CS
 		for (int i = 0; i < 300; i++)
 			globalData[i] = globalData[i] + 1;
 		atomic_fetch_add(&counter, 1);
-		// atomic_fetch_add(&counter, 1);
 		// CS
 		spin_unlock();  // unlock
+
 		clock_gettime(CLOCK_REALTIME, &rs_start);
-		while (clock_gettime(CLOCK_REALTIME, &current) && time_diff(rs_start, current).tv_nsec > rs)
-			;
-		diff = time_diff(start, current).tv_sec;
-	}
+	    while(1){
+            clock_gettime(CLOCK_REALTIME, &rs_end);
+            if(time_diff(rs_start, rs_end).tv_nsec > rs)
+                break;
+        }
+    }
 }
 
 int main() {
@@ -71,6 +74,8 @@ int main() {
 			args[i].rs_label = j;
 			pthread_create(&tid[i], NULL, (void*)thread, &args[i]);
 		}
+
+        clock_gettime(CLOCK_REALTIME, &start);
 		// pthread_spin_unlock(&lock);
 		for (int i = 0; i < num_of_thread; i++)
 			pthread_join(tid[i], NULL);
