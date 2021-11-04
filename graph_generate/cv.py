b81e4bad3fe0c37ffed623371b7294b379ed7bb5
playone <kpu107u@csie.io>
2021-11-04 15:38:40 +0800

    coefficient of variation

---
+++ graph_generate/cv.py
@@ -0,0 +1,75 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# cv = σ/μ
+# σ = sqrt((sum(Xi - μ))^2/amount)
+# μ = average
+
+def cal(filename):
+    # 160000, 120000, 80000, 40000, 20000, 10000, 5000, 2500, 1000, 500, 100
+    dt_amount = 11
+    count = 0
+    sum = np.zeros(11)
+    avg = np.zeros(11)
+    S_D = np.zeros(11)
+    CV = np.zeros(11)
+        
+    # calculate the average
+    with open(filename, 'r') as f:
+        for line in f:
+            parse = line.split()
+            value = (int)(parse[1])
+            index = count % dt_amount
+            sum[index] += value
+            count += 1
+            
+    times = count / dt_amount
+    for i in range(dt_amount):
+        avg[i] = sum[i] / times
+
+    # calculate the standard deriviation
+    count = 0
+    with open('SoA_lps', 'r') as f:
+        for line in f:
+            parse = line.split()
+            index = count % dt_amount
+            value = (int)(parse[1])
+            S_D[index] += ((value - avg[index]) ** 2)
+            count += 1
+
+    for i in range(dt_amount):
+        S_D[i] /= times
+        S_D[i] = np.sqrt(S_D[i])
+    for i in range(dt_amount):
+        print(i, "\t", S_D[i], "\t", avg[i])
+
+    # calculate CV
+    for i in range(dt_amount):
+        CV[i] = (S_D[i] / avg[i]) * 100
+
+    return CV
+    
+def main():
+    x_axis = ["160k", "120k", "80k", "40k", "20k", "10k", "5k", "2.5k", "1k", "0.5k", "0.1k"]
+    y_axis = [0, 20, 40, 80, 100, 120]
+    dt_amount = 11
+
+    SoA_CV = cal('SoA_lps')
+    plock_CV = cal('plock_lps')
+    MCS_CV = cal('mcs_lps')
+    tlock_CV = cal('ticket_lps')
+
+    plt.figure(figsize = (15, 10), dpi = 100, linewidth = 2)
+    plt.plot(x_axis, SoA_CV, 's-', color = 'r', label = "SoA")
+    plt.plot(x_axis, plock_CV, 'o-', color = 'g', label = "plock")
+    plt.plot(x_axis, MCS_CV, 'x-', color = 'b', label = "MCS")
+    plt.plot(x_axis, tlock_CV, '^-', color = 'y', label = "tlock")
+
+    plt.title('coefficient of variation (CV)')
+    plt.xlabel('Remainder Section Size', fontsize = 30, labelpad = 15)
+    plt.legend(loc = "best", fontsize = 20)
+    plt.grid()
+    plt.show()
+
+if __name__ == '__main__':
+    main()
