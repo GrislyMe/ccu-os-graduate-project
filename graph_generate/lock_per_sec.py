66ddb18df207bdcec7a0229eeb61a0c6655b1d5b
GrislyMe <grislyme@csie.io>
2021-11-06 16:03:43 +0800

    graph gen update and chmod

--- graph_generate/lock_per_sec.py
+++ graph_generate/lock_per_sec.py
@@ -38,6 +38,10 @@ def main():
     line = plt.plot(xi, y, label = "mcs_spinlock")
     plt.setp(line, marker = "x")
 
+    y = getCost("../plock/plock_lps.bak")
+    line = plt.plot(xi, y, label = "plock_lps_old")
+    plt.setp(line, marker = "v")
+
     plt.title("Lock Per Sec")
     plt.xticks(xi, x)
     plt.xlabel("rs_size")
